999b23227b1e4e601ac5dbcadd9dbce1554d8c3c
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-19 15:17:16 -0300

    class 'pegando elementos filhos' done

--- Preparação/Pilotando com a DOM/navegandoPelosElementos.js
+++ Preparação/Pilotando com a DOM/navegandoPelosElementos.js
@@ -1,6 +1,17 @@
 //ParentElement e ParentNode
 
 let element = document.querySelector('h1');
-
 console.log(element.parentElement);
 console.log(element.parentNode);
+
+//pegando elementos filhos
+
+element = document.querySelector('body');
+console.log(element.childNodes);
+console.log(element.children);
+//firstChild firstElementChild
+console.log(element.firstChild);
+console.log(element.firstElementChild);
+//lastChild lastElementChild
+console.log(element.lastChild);
+console.log(element.lastElementChild);
