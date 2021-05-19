0186b585bcfda0626298f88cf040c5330d106a75
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-19 12:19:49 -0300

    class 'classList' done.

--- Preparação/Pilotando com a DOM/manipulandoEstilosEClasses.js
+++ Preparação/Pilotando com a DOM/manipulandoEstilosEClasses.js
@@ -1,4 +1,14 @@
-//Auterando estilos com style
+//Alterando estilos com style
 let element = document.querySelector('body');
 
-element.style.backgroundColor = "#44ccff"
+//element.style.backgroundColor = "#44ccff";
+
+//classList
+
+
+
+element.classList.add('active', 'green');
+element.classList.remove('green');
+element.classList.toggle('green');
+
+console.log(element.classList);
