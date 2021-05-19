64bd757537cfb19912011819144d6935df0c8e35
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-19 15:24:03 -0300

    class 'buscando irmãos' done

--- Preparação/Pilotando com a DOM/navegandoPelosElementos.js
+++ Preparação/Pilotando com a DOM/navegandoPelosElementos.js
@@ -15,3 +15,13 @@ console.log(element.firstElementChild);
 //lastChild lastElementChild
 console.log(element.lastChild);
 console.log(element.lastElementChild);
+
+//buscando irmãos
+element = document.querySelector('header');
+//nextSibling e nextElementSibling
+console.log(element.nextSibling);
+console.log(element.nextElementSibling);
+//previousSibling e previousElementSibling
+element = document.querySelector('body script')
+console.log(element.previousSibling);
+console.log(element.previousElementSibling);
