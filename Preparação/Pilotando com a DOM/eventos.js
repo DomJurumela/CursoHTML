fd2f36793a9f4968a60702aa02a352ba2ee5dd32
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-19 15:49:16 -0300

    class 'Eventos de teclado' done

--- Preparação/Pilotando com a DOM/eventos.js
+++ Preparação/Pilotando com a DOM/eventos.js
@@ -2,3 +2,12 @@
 function print(){
     console.log("print")
 }
+
+//Eventos de teclado
+let input = document.querySelector('input');
+
+input.onkeydown = function rodar(){
+    console.log('rodei')
+}
+
+//Eventos via js
