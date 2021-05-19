62bc6273ed7af4237a033a9e29702cf2f2272557
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-19 15:53:14 -0300

    class 'Adcionando eventos via JS' done

--- Preparação/Pilotando com a DOM/eventos.js
+++ Preparação/Pilotando com a DOM/eventos.js
@@ -4,10 +4,14 @@ function print(){
 }
 
 //Eventos de teclado
-let input = document.querySelector('input');
+const input = document.querySelector('input');
 
 input.onkeydown = function rodar(){
     console.log('rodei')
 }
 
 //Eventos via js
+
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('mouseover', print);
