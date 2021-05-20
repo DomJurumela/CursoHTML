87f101a299f3c958f136466d8ca4c15bf20fccd5
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-20 09:16:49 -0300

    Exercise 'desafio modal' done.

---
+++ Preparação/Pilotando com a DOM/Exercicios.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('openModal');
+const telaSecundaria = document.getElementById('tela2');
+
+
+document.addEventListener('keydown', fecharTela);
+button.addEventListener('click', abrirTela)
+
+function abrirTela(){
+    telaSecundaria.classList.remove('invisible');
+}
+
+function fecharTela(k){
+    //k é o nome do evento criado para detectar o pressionamento de botão
+
+    key = k.keyCode;
+    //key é o valor da tecla pressionada, "Esc" seria igual a 27
+    console.log(key);
+    if (key == "27" && !telaSecundaria.classList.contains('invisible')){ //checando o valor de key e se a tela secundária tem o atributo invisible
+        telaSecundaria.classList.add('invisible');
+    };
+}
+
+document.onkeypress = function(event){
+    console.log(event.key)
+}
+
