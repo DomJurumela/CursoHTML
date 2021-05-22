d3d670d9d80bc0ae9c109c8603b8f9f80a082aa5
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-22 08:42:55 -0300

    Class 'Executando o primeiro arquivo e entendendo globals' done.

---
+++ Viajando/iniciandoNaPratica.js
@@ -0,0 +1,7 @@
+//Executando o primeiro arquivo e entendendo globals
+
+console.log('Alguma coisa aqui');
+global.console.log('Alguma coisa aqui');
+console.log(global);
+console.log(__dirname);
+console.log(__filename);
