4da3849e65531bb4d5d7044a0e6134a8af74afd8
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-22 08:56:29 -0300

    Class 'O que são e como utilizar módulos' done.

--- Viajando/iniciandoNaPratica.js
+++ Viajando/iniciandoNaPratica.js
@@ -1,7 +1,16 @@
 //Executando o primeiro arquivo e entendendo globals
-
+console.log("Executando o primeiro arquivo e Entendendo globals: \n");
 console.log('Alguma coisa aqui');
 global.console.log('Alguma coisa aqui');
 console.log(global);
 console.log(__dirname);
 console.log(__filename);
+
+console.log("\n\n\n");
+
+//O que são e como utilizar módulos
+console.log("O que são e como utilizar módulos: \n");
+
+const path = require('path');
+console.log(path);
+console.log(path.basename(__filename));
