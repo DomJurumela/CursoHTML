e15c993360391442f67d22a68f48d0acc312882b
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-24 15:09:22 -0300

    class 'Finalizando e Revisando' done.

--- Viajando/NodeJs/bestMe.js
+++ Viajando/NodeJs/bestMe.js
@@ -3,7 +3,6 @@
 const questions = [
     "O que aprendi hoje?",
     "O que me deixou aborrecido?",
-    "O que eu poderia fazer para melhorar?",
     "O que me deixou feliz hoje?",
     "Quantas pessoas ajudei hoje?"
 ]
@@ -24,4 +23,21 @@ process.stdin.on("data", data => {
         console.log(answers);
         process.exit();
     }
+})
+
+process.on('exit', () =>{
+    console.log(`
+    Bacana!
+
+    O que você aprendeu hoje foi:
+    ${answers[0]}
+
+    O que te aborreceu foi:
+    ${answers[1]}
+
+    O que te deixou feliz hoje:
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!
+    `)
 })
