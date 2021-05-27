76aeea4bd01c035836819c45bd2b5e29304b8603
Joao Jurema <joaoalvesjurema@gmail.com>
2021-05-27 09:53:33 -0300

    class 'forEach' done.

--- Viajando/NodeJS-EJS/server.js
+++ Viajando/NodeJS-EJS/server.js
@@ -3,13 +3,45 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res){
-    res.render("pages/index");
+app.get("/", function (req, res) {
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "morzinho"
+        },
+        {
+            title: "I",
+            message: "nstall EJS"
+
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML utilizando JavaScript"
+    res.render("pages/index", {qualities: items, subtitle: subtitle});
 })
 
-app.get("/sobre", function (req, res){
+app.get("/sobre", function (req, res) {
     res.render("pages/about");
 })
 
 app.listen(8080);
 console.log("Servidor ativo...");
+
+//Lembrete: Para rodar o servidor, execute "node server" no terminal e entre em "localhost:8080" no navegador
